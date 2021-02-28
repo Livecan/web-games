ea07df02e32f6caa024a8ed46f9926de9ee9d150
Roman <roman.meszaros@gmail.com>
2021-02-28 21:09:28 +0300

    FEATURE Reloading board - Updating token positions via AJAX.

--- templates/DrowningGames/open_reload_board.php
+++ templates/DrowningGames/open_reload_board.php
@@ -11,7 +11,7 @@ $this->Html->css('drowning-game/board', ['block' => true]);
 </div>
 <div id="ocean">
     <?php for ($i = 1; $i <= 20; $i++): ?>
-    <div class="depth">
+    <div id="depth<?= $i ?>" class="depth">
         <div class="tokens">
         </div>
     </div>
@@ -27,6 +27,14 @@ $this->Html->css('drowning-game/board', ['block' => true]);
             $.getJSON('<?= \Cake\Routing\Router::url(['action' => 'update-board-json', $game->id]) ?>',
                     function(data, status){
                         alert("Status: " + status + "\nData: " + JSON.stringify(data));
+                        for (let depthNo in data["depths"]) {
+                            let jsonTokens = data["depths"][depthNo].tokens;
+                            let tokens = "";
+                            for (let token in jsonTokens) {
+                                tokens += '<div class="token T' + jsonTokens[token]["type"] + '"></div>';
+                            }
+                            $("#depth" + depthNo + " .tokens").html(tokens);
+                        }
             });
         });
     });
