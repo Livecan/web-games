b2addc7c640ea8f83068c24e46f972f08608f236
Roman <roman.meszaros@gmail.com>
2021-03-27 18:44:38 +0200

    REFACTORING

--- src/Controller/FormulaController.php
+++ src/Controller/FormulaController.php
@@ -16,34 +16,6 @@ class FormulaController extends AppController
         $this->Formula = new \App\Model\Logic\FormulaLogic();
         $this->FormulaGame = $this->loadModel('FormulaGames');
     }
-    
-    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|null|void Renders view
-     */
-    public function index()
-    {
-        //$formula = $this->paginate($this->Formula);
-
-        $this->set(compact('formula'));
-    }
-
-    /**
-     * View method
-     *
-     * @param string|null $id Formula id.
-     * @return \Cake\Http\Response|null|void Renders view
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $formula = $this->Formula->get($id, [
-            'contain' => [],
-        ]);
-
-        $this->set(compact('formula'));
-    }
 
     /**
      * Start method
@@ -64,49 +36,5 @@ class FormulaController extends AppController
             $this->Flash->error(__('The formula could not be saved. Please, try again.'));
         }
         $this->set(compact('formula'));
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id Formula id.
-     * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $formula = $this->Formula->get($id, [
-            'contain' => [],
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $formula = $this->Formula->patchEntity($formula, $this->request->getData());
-            if ($this->Formula->save($formula)) {
-                $this->Flash->success(__('The formula has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The formula could not be saved. Please, try again.'));
-        }
-        $this->set(compact('formula'));
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Formula id.
-     * @return \Cake\Http\Response|null|void Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $formula = $this->Formula->get($id);
-        if ($this->Formula->delete($formula)) {
-            $this->Flash->success(__('The formula has been deleted.'));
-        } else {
-            $this->Flash->error(__('The formula could not be deleted. Please, try again.'));
-        }
-
-        return $this->redirect(['action' => 'index']);
     }
 }
