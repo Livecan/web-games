d4e4d966096f3476602effa57c64b11234a873c9
Roman <roman.meszaros@gmail.com>
2021-04-22 10:45:31 +0200

    FORMULA REFACTORING

--- src/Model/FormulaLogic/FormulaLogic.php
+++ src/Model/FormulaLogic/FormulaLogic.php
@@ -34,44 +34,6 @@ class FormulaLogic {
         $this->MovementLogic = new MovementLogic();
     }
     
-    /*public function start(FormulaGame $formulaGame) {
-        $formulaGame->fo_cars = collection($formulaGame->users)->map(function($user) use ($formulaGame) {
-            $playerCarsLeft = $formulaGame->fo_game->cars_per_player;
-            while ($playerCarsLeft-- > 0) {
-                $damages = $this->FoDamages->newEntities([
-                        ['fo_e_damage_type_id' => 1, 'wear_points' => 7],
-                        ['fo_e_damage_type_id' => 2, 'wear_points' => 3],
-                        ['fo_e_damage_type_id' => 3, 'wear_points' => 3],
-                        ['fo_e_damage_type_id' => 4, 'wear_points' => 3],
-                        ['fo_e_damage_type_id' => 5, 'wear_points' => 3],
-                        ['fo_e_damage_type_id' => 6, 'wear_points' => 3],]);
-                yield $this->FoCars->createUserCar($formulaGame->id, $user->id, $damages, false);
-            }
-        })->unfold()->shuffle();
-        
-        $startingPositions = $this->FoPositions->find('all')->
-                select('id')->
-                where(['fo_track_id' => $formulaGame->fo_game->fo_track_id])->
-                whereNotNull('starting_position')->
-                order(['starting_position' => 'ASC']);
-        $formulaGame->fo_cars = $formulaGame->fo_cars->zip($startingPositions)->
-                map(function($carPositionPair) {
-                    $carPositionPair[0]->fo_position_id = $carPositionPair[1]->id;
-                    return $carPositionPair[0];
-                })->toList();
-        
-        $formulaGame->game_state_id = 2;
-        
-        $formulaGame->setDirty('fo_card',true);
-        $formulaGame = $this->FormulaGames->save($formulaGame,
-                        ['associated' => ['FoCars', 'FoCars.FoDamages']]);
-        $formulaGame->fo_cars = $this->FoCars->generateCarOrder($formulaGame->id)->toList();
-        
-        $this->FoLogs->logGameStart($formulaGame->fo_cars);
-        
-        return $formulaGame;
-    }*/
-    
     public function getBoard(FormulaGame $formulaGame, $user_id = null, Time $modifiedDateParam = null) {
         $foLogLastModified = $this->FoLogs->find('all')->
                 contain(['FoCars'])->
