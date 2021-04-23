3bb3e0b9d6b318dfdb33b01a85ae5724ec311658
Roman <roman.meszaros@gmail.com>
2021-04-23 13:36:17 +0200

    DB backup.

--- backups/games27-update.sql
+++ backups/games27-update.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `games`.`fo_move_options` ADD `is_next_lap` BOOLEAN NOT NULL DEFAULT FALSE AFTER `fo_position_id`;
 ALTER TABLE `games_users` CHANGE `ready_state` `ready_state` CHAR(1) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT 'N' COMMENT 'R - ready,\r\nN - not ready';
-UPDATE `games_users` SET `ready_state`='N'
+UPDATE `games_users` SET `ready_state`='N';
+INSERT INTO `fo_position2positions` (`id`, `fo_position_from_id`, `fo_position_to_id`, `is_left`, `is_straight`, `is_right`, `is_curve`, `is_adjacent`, `is_equal_distance`, `is_pitlane_move`) VALUES (NULL, '392', '396', '0', '0', '0', '1', '1', '0', '0');
+ALTER TABLE `users` ADD `is_beta` BOOLEAN NOT NULL DEFAULT FALSE AFTER `is_admin`;
