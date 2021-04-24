f54be756cd53b32cf8fa95750b1b17bdf47feeda
Roman <roman.meszaros@gmail.com>
2021-04-24 15:28:49 +0200

    DB FIX - Changed order number for final positions in Monaco.

--- backups/games27-update.sql
+++ backups/games27-update.sql
@@ -3,3 +3,4 @@ ALTER TABLE `games_users` CHANGE `ready_state` `ready_state` CHAR(1) CHARACTER S
 UPDATE `games_users` SET `ready_state`='N';
 INSERT INTO `fo_position2positions` (`id`, `fo_position_from_id`, `fo_position_to_id`, `is_left`, `is_straight`, `is_right`, `is_curve`, `is_adjacent`, `is_equal_distance`, `is_pitlane_move`) VALUES (NULL, '392', '396', '0', '0', '0', '1', '1', '0', '0');
 ALTER TABLE `users` ADD `is_beta` BOOLEAN NOT NULL DEFAULT FALSE AFTER `is_admin`;
+UPDATE `fo_positions` SET `order`=`order`-515 WHERE id IN (512, 513, 514, 515);
