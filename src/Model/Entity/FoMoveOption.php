584960837e8b6bb892c90a10e8f0bef069d7c60b
Roman <roman.meszaros@gmail.com>
2021-05-13 12:16:32 +0200

    FORMULA FIX

--- src/Model/Entity/FoMoveOption.php
+++ src/Model/Entity/FoMoveOption.php
@@ -180,7 +180,7 @@ class FoMoveOption extends Entity
                     return $q->where(['FoCars.game_id' => $this->fo_car->game_id]);
                 }])->
                 where(['fo_position_from_id' => $this->fo_position_id,
-                    'is_straight' => true])->first()->fo_position_to->fo_cars;
+                    'is_straight' => true])->first();
         $carInFront = $foPosition2Position->has('fo_position_to') ?
                 $foPosition2Position->fo_position_to->fo_cars[0] :
                 null;
