963134d0b9f14868c184b98127ca8ce7f06a9f5c
Livecan <roman.meszaros@gmail.com>
2021-08-04 10:56:13 +0100

    UI Tooltip.

--- webroot/js/module/tooltip.js
+++ webroot/js/module/tooltip.js
@@ -7,32 +7,29 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 export var Tooltip = function (_React$Component) {
-  _inherits(Tooltip, _React$Component);
+    _inherits(Tooltip, _React$Component);
 
-  function Tooltip() {
-    _classCallCheck(this, Tooltip);
+    function Tooltip() {
+        _classCallCheck(this, Tooltip);
 
-    return _possibleConstructorReturn(this, (Tooltip.__proto__ || Object.getPrototypeOf(Tooltip)).apply(this, arguments));
-  }
-
-  _createClass(Tooltip, [{
-    key: "render",
-    value: function render() {
-      //TODO: figure out which params should go to CSS and do styling
-      return React.createElement(
-        "span",
-        { style: {
-            position: "fixed",
-            display: "inline-block",
-            left: this.props.x,
-            top: this.props.y,
-            width: 100,
-            backgroundColor: "white",
-            zIndex: 1000000 } },
-        this.props.text
-      );
+        return _possibleConstructorReturn(this, (Tooltip.__proto__ || Object.getPrototypeOf(Tooltip)).apply(this, arguments));
     }
-  }]);
 
-  return Tooltip;
+    _createClass(Tooltip, [{
+        key: "render",
+        value: function render() {
+            //TODO: figure out which params should go to CSS and do styling
+            return React.createElement(
+                "div",
+                { className: "tooltip",
+                    style: {
+                        position: "fixed",
+                        left: this.props.x,
+                        top: this.props.y } },
+                this.props.children
+            );
+        }
+    }]);
+
+    return Tooltip;
 }(React.Component);
