ac22780a9bb33f659502ea1b06f0688fae99f801
Livecan <roman.meszaros@gmail.com>
2021-08-16 18:44:16 +0100

    FORMULA DB FIX Adding missing position2position.

--- backups/games31-update.sql
+++ backups/games31-update.sql
@@ -8,3 +8,25 @@ DELETE FROM `fo_position2positions` WHERE EXISTS
  ALTER TABLE `fo_damages` DROP FOREIGN KEY `fo_damages_ibfk_3`; ALTER TABLE `fo_damages` ADD CONSTRAINT `fo_damages_ibfk_3` FOREIGN KEY (`fo_log_id`) REFERENCES `fo_logs`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 ALTER TABLE `fo_damages` DROP FOREIGN KEY `fo_damages_ibfk_1`; ALTER TABLE `fo_damages` ADD CONSTRAINT `fo_damages_ibfk_1` FOREIGN KEY (`fo_car_id`) REFERENCES `fo_cars`(`id`) ON DELETE CASCADE ON UPDATE RESTRICT; ALTER TABLE `fo_damages` DROP FOREIGN KEY `fo_damages_ibfk_3`; ALTER TABLE `fo_damages` ADD CONSTRAINT `fo_damages_ibfk_3` FOREIGN KEY (`fo_log_id`) REFERENCES `fo_logs`(`id`) ON DELETE CASCADE ON UPDATE RESTRICT; ALTER TABLE `fo_damages` DROP FOREIGN KEY `fo_damages_ibfk_4`; ALTER TABLE `fo_damages` ADD CONSTRAINT `fo_damages_ibfk_4` FOREIGN KEY (`fo_move_option_id`) REFERENCES `fo_move_options`(`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
 ALTER TABLE `fo_cars` ADD `pits_state` CHAR(1) NULL DEFAULT NULL COMMENT 'P - in pits, L - long stop' AFTER `state`;
+
+INSERT
+INTO `fo_position2positions` (
+    `fo_position_from_id`,
+    `fo_position_to_id`,
+    `is_left`,
+    `is_straight`,
+    `is_right`,
+    `is_curve`,
+    `is_adjacent`,
+    `is_equal_distance`,
+    `is_pitlane_move`)
+SELECT MAX(f1.`id`),
+    (SELECT MAX(f2.`id`) FROM `fo_positions` f2 WHERE f2.`order` = 106 AND f2.`fo_track_id` = 2),
+    '0',
+    '1',
+    '0',
+    '0',
+    '1',
+    '0',
+    '0'
+FROM `fo_positions` f1 WHERE f1.`order` = 101 AND f1.`fo_track_id` = 2;
